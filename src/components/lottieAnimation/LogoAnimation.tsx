12b76939cb625e5dfac238bdd8dbcb8dce79ce79
Dev-Surjendu <rahulkar9988@gmail.com>
2024-02-08 20:02:51 +0530

    Nabar/responsive

--- src/components/lottieAnimation/LogoAnimation.tsx
+++ src/components/lottieAnimation/LogoAnimation.tsx
@@ -28,3 +28,53 @@ const LogoAnimation = () => {
 };
 
 export default LogoAnimation;
+
+// import React, { useRef, useEffect } from "react";
+// import Lottie from "lottie-web";
+// import animationData from "../../assets/Animation - 1705777251875.json";
+// import { useTheme } from "@mui/material/styles";
+// import useMediaQuery from "@mui/material/useMediaQuery";
+
+// const LogoAnimation = () => {
+//   const animBox = useRef(null);
+//   const theme = useTheme();
+//   const isLg = useMediaQuery(theme.breakpoints.down("lg"));
+//   const isMd = useMediaQuery(theme.breakpoints.down("md"));
+//   const isSm = useMediaQuery(theme.breakpoints.down("sm"));
+
+//   useEffect(() => {
+//     if (animBox.current) {
+//       Lottie.loadAnimation({
+//         container: animBox.current,
+//         renderer: "svg",
+//         loop: true,
+//         autoplay: true,
+//         animationData: animationData,
+//       });
+//     }
+
+//     // Cleanup function
+//     return () => {
+//       if (animBox.current) {
+//         Lottie.destroy();
+//       }
+//     };
+//   }, []);
+
+//   // Dynamically adjust the size of the Lottie container
+//   const getAnimationBoxStyle = () => {
+//     if (isLg) {
+//       return { width: "50px", height: "50px" };
+//     } else if (isMd) {
+//       return { width: "10px", height: "10px" };
+//     } else if (isSm) {
+//       return { width: "10px", height: "10px" };
+//     } else {
+//       return { width: "50px", height: "55px" };
+//     }
+//   };
+
+//   return <div style={getAnimationBoxStyle()} ref={animBox} />;
+// };
+
+// export default LogoAnimation;
