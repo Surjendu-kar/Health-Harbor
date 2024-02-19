b1b2c04046151f28e67872da50070d9c79eb5b85
Dev-Surjendu <rahulkar9988@gmail.com>
2024-02-20 00:46:32 +0530

    add searchbar

--- src/pages/find a doctor/FindDoctor.tsx
+++ src/pages/find a doctor/FindDoctor.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import { Box, Input, Select, MenuItem, Button, styled } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const MainContainer = styled(Box)(() => ({
+  // You can add styles to the MainContainer if needed
+}));
 
 function FindDoctor() {
   return (
-    <div>FindDoctor</div>
-  )
+    <MainContainer>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          // backgroundColor: "transparent",
+        }}
+      >
+        <Box
+          sx={{
+            padding: "3px",
+            border: "1px solid black",
+            borderRadius: "20px",
+          }}
+        >
+          <Input placeholder="Type something..." disableUnderline />
+          <span>| </span>
+          <Select
+            defaultValue=""
+            displayEmpty
+            input={<Input disableUnderline />}
+            renderValue={(selected) => {
+              if (selected.length === 0) {
+                return <span>Select an option</span>;
+              }
+              return selected;
+            }}
+          >
+            <MenuItem value="">
+              <em>Select an option</em>
+            </MenuItem>
+            <MenuItem value="cardiology">Cardiology</MenuItem>
+            <MenuItem value="dentist">Dentist</MenuItem>
+            <MenuItem value="specialized">Specialized</MenuItem>
+            <MenuItem value="urology">Urology</MenuItem>
+            <MenuItem value="neurology">Neurology</MenuItem>
+            <MenuItem value="orthopedic">Orthopedic</MenuItem>
+            <MenuItem value="stomach">Stomach</MenuItem>
+          </Select>
+
+          <Button
+            variant="contained"
+            startIcon={<SearchIcon />}
+            sx={{
+              backgroundColor: "#1D2B53",
+              "&:hover": { backgroundColor: "#16324C" },
+              borderRadius: "20px",
+            }}
+          >
+            Search
+          </Button>
+        </Box>
+      </Box>
+    </MainContainer>
+  );
 }
 
-export default FindDoctor
+export default FindDoctor;
