80de767527fc45a5c8cf6c58c82585eefd403432
Dev-Surjendu <rahulkar9988@gmail.com>
2024-02-25 11:59:21 +0530

    -Divide OverView -Add ViewDetails

--- src/pages/profile/overview/OverView.tsx
+++ src/pages/profile/overview/OverView.tsx
@@ -4,6 +4,7 @@ import { User } from "@supabase/supabase-js";
 import { supabase } from "../../../supabase/config";
 import { useEffect, useRef, useState } from "react";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import ViewDetails from "../../../components/viewDetails/ViewDetails";
 
 const MainContainer = styled(Box)(({ theme }) => ({
   width: "50%",
@@ -115,92 +116,7 @@ const Rating = styled(Typography)(({ theme }) => ({
   [theme.breakpoints.down("sm")]: {},
 }));
 
-const Contain = styled(Typography)(() => ({
-  margin: "1rem 0",
-}));
 
-const Heading = styled(Typography)(({ theme }) => ({
-  fontSize: "1.05rem",
-  marginBottom: "0.5rem",
-
-  [theme.breakpoints.down("lg")]: {
-    fontSize: "0.95rem",
-    marginBottom: "0.3rem",
-  },
-  [theme.breakpoints.down("md")]: {
-    fontSize: "0.85rem",
-    marginBottom: "0.2rem",
-  },
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "0.55rem",
-    marginBottom: "0.1rem",
-  },
-}));
-
-const Title = styled(Typography)(({ theme }) => ({
-  fontSize: "0.85rem",
-  [theme.breakpoints.down("lg")]: { fontSize: "0.7rem" },
-  [theme.breakpoints.down("md")]: { fontSize: "0.6rem" },
-  [theme.breakpoints.down("sm")]: { fontSize: "0.35rem" },
-}));
-
-const EducationBox = styled(Box)(({ theme }) => ({
-  padding: "1rem 7rem 1rem 3rem",
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  backgroundColor: "#deeaff8f",
-  borderRadius: "10px",
-  marginTop: "1rem",
-
-  [theme.breakpoints.down("lg")]: {
-    padding: "0.8rem 4.5rem 0.8rem 2rem",
-    marginTop: "0.7rem",
-  },
-  [theme.breakpoints.down("md")]: {
-    padding: "0.7rem 1.5rem 0.7rem 1.5rem",
-    marginTop: "0.5rem",
-    borderRadius: "5px",
-  },
-  [theme.breakpoints.down("sm")]: {
-    padding: "0.3rem 1rem 0.3rem 1rem",
-    marginTop: "0.3rem",
-  },
-}));
-
-const ExperienceBox = styled(Box)(({ theme }) => ({
-  backgroundColor: "#deeaff8f",
-  borderRadius: "10px",
-  padding: "1.5rem 7rem 1.5rem 3rem",
-  marginBottom: "0.3rem",
-
-  [theme.breakpoints.down("lg")]: {
-    padding: "1.15rem 4.5rem 1.15rem 2rem",
-  },
-  [theme.breakpoints.down("md")]: {
-    padding: "1rem 1.3rem 1rem 1.5rem",
-    borderRadius: "5px",
-  },
-  [theme.breakpoints.down("sm")]: {
-    padding: "0.3rem 1rem 0.3rem 1rem",
-    flexDirection: "column",
-    alignItems: "start",
-    width: "100%",
-    ".MuiTypography-root": {
-      // Target all Typography components inside ExperienceBox
-      textAlign: "left",
-      width: "100%",
-    },
-  },
-}));
-
-const DateText = styled(Typography)(({ theme }) => ({
-  fontSize: "0.9rem",
-  fontWeight: "bold",
-  [theme.breakpoints.down("lg")]: { fontSize: "0.8rem" },
-  [theme.breakpoints.down("md")]: { fontSize: "0.7rem" },
-  [theme.breakpoints.down("sm")]: { fontSize: "0.45rem" },
-}));
 
 type DoctorInfo = {
   id: number;
@@ -357,73 +273,12 @@ function OverView({
       </ImgBox>
 
       {user && fetchedData && (
-        <Box>
-          {/* About */}
-          <Contain>
-            <Heading>About</Heading>
-            <Title>{fetchedData.about}</Title>
-          </Contain>
-
-          {/* Education */}
-          <Contain>
-            <Heading>Education</Heading>
-            <Box>
-              {qualificationsArray &&
-                qualificationsArray.map((qualification, index) => (
-                  <EducationBox key={index}>
-                    <DateText sx={{ color: "#009cff" }}>
-                      {new Date(qualification.startDate).toLocaleDateString(
-                        "en-US",
-                        { year: "numeric", month: "short" }
-                      )}{" "}
-                      -{" "}
-                      {qualification.endDate
-                        ? new Date(qualification.endDate).toLocaleDateString(
-                            "en-US",
-                            { year: "numeric", month: "short" }
-                          )
-                        : "Present"}
-                    </DateText>
-                    <Title>
-                      {qualification.degree}, {qualification.university}
-                    </Title>
-                  </EducationBox>
-                ))}
-            </Box>
-          </Contain>
-
-          {/* Experience */}
-          <Contain>
-            <Heading>Experience</Heading>
-            <Box
-              sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-between",
-              }}
-            >
-              {experiencesArray.map((experience, index) => (
-                <ExperienceBox key={index} sx={{}}>
-                  <DateText sx={{ color: "#009cff" }}>
-                    {new Date(experience.startDate).toLocaleDateString(
-                      "en-US",
-                      { year: "numeric", month: "short" }
-                    )}{" "}
-                    -{" "}
-                    {experience.endDate
-                      ? new Date(experience.endDate).toLocaleDateString(
-                          "en-US",
-                          { year: "numeric", month: "short" }
-                        )
-                      : "Present"}
-                  </DateText>
-                  <Title>{experience.position}</Title>
-                  <Title>{experience.hospital}</Title>
-                </ExperienceBox>
-              ))}
-            </Box>
-          </Contain>
-        </Box>
+        <ViewDetails
+          user={user}
+          fetchedData={fetchedData}
+          qualificationsArray={qualificationsArray}
+          experiencesArray={experiencesArray}
+        />
       )}
     </MainContainer>
   );
