e2ad33fc1787809079d4e294420ba48e4a4fbabd
Dev-Surjendu <rahulkar9988@gmail.com>
2024-02-29 20:56:14 +0530

    Delete the DoctorDetails path

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,6 @@ const App = () => {
         <Route element={<LoginWithGoogle />} path="/login" />
         <Route element={<Signup />} path="/signup" />
         <Route element={<FindDoctor />} path="/find-a-doctor" />
-        <Route element={<DoctorDetails />} path="/doctor-details" />
         <Route element={<Service />} path="/service" />
         <Route element={<AboutUs />} path="/about-us" />
         <Route element={<Contact />} path="/contact" />
