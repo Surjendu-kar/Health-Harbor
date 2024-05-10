7d5b3a5940781631a8ceffac4caf0d32133959de
Surjendu kar <rahulgotp9988@gmail.com>
2024-05-10 19:40:44 +0530

    store appointmet into the patientInfo dabase

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -134,7 +134,7 @@ const Navbar = () => {
     const authListener = supabase.auth.onAuthStateChange((event, session) => {
       if (event === "SIGNED_IN") {
         setUser(session?.user ?? null);
-        console.log(session?.user);
+        // console.log(session?.user);
 
         fetchDoctorImage(session?.user.email);
       } else if (event === "SIGNED_OUT") {
@@ -201,3 +201,214 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+//below we fixed the avatar img prb but there is another prb occur 
+
+// import { Box, Avatar } from "@mui/material";
+// import { styled } from "@mui/system";
+// import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import { supabase } from "../../supabase/config";
+// import { User } from "@supabase/supabase-js";
+// import LogoAnimation from "../lottieAnimation/LogoAnimation";
+
+// const NavContainer = styled(Box)(() => ({
+//   display: "flex",
+//   justifyContent: "space-around",
+//   alignItems: "center",
+//   padding: "0.5rem 0",
+//   backgroundColor: "#1D2B53",
+//   width: "100%",
+// }));
+
+// const StyledLink = styled(Link)(({ theme }) => ({
+//   fontFamily: "sans-serif",
+//   margin: theme.spacing(0, 2),
+//   textDecoration: "none",
+//   color: "#fff",
+//   fontSize: "0.9  rem",
+//   letterSpacing: "1.5px",
+
+//   transition: "color 0.3s ease-in-out, font-size 0.3s ease-in-out",
+//   "&:hover": {
+//     fontSize: "1.05rem",
+//   },
+
+//   [theme.breakpoints.down("lg")]: {
+//     fontSize: "0.9rem",
+//     "&:hover": {
+//       fontSize: "1rem",
+//     },
+//   },
+//   [theme.breakpoints.down("md")]: {
+//     fontSize: "0.8rem",
+//     letterSpacing: "1.2px",
+//     "&:hover": {
+//       fontSize: "0.9rem",
+//     },
+//     margin: theme.spacing(0, 1),
+//   },
+//   [theme.breakpoints.down("sm")]: {
+//     fontSize: "0.53rem",
+//     letterSpacing: "0.7px",
+//     margin: theme.spacing(0, 0.5),
+
+//     "&:hover": {
+//       fontSize: "0.65rem",
+//     },
+//   },
+// }));
+
+// const MedicineStyle = styled(Box)(({ theme }) => ({
+//   display: "flex",
+//   alignItems: "center",
+//   width: "50px",
+//   height: "55px",
+//   [theme.breakpoints.down("lg")]: {
+//     width: "50px",
+//     height: "50px",
+//   },
+//   [theme.breakpoints.down("md")]: {
+//     width: "30px",
+//     height: "30px",
+//   },
+//   [theme.breakpoints.down("sm")]: { width: "27px", height: "27px" },
+// }));
+
+// const LogoLink = styled(StyledLink)(({ theme }) => ({
+//   fontSize: "1.15rem",
+//   margin: theme.spacing(0, 0.05),
+//   position: "relative",
+//   "&::after": {
+//     content: '""',
+//     position: "absolute",
+//     left: 0,
+//     bottom: 0,
+//     height: "1px",
+//     width: "0%",
+//     backgroundColor: "#fff",
+//     transition: "all ease 1s",
+//   },
+
+//   "&:hover::after": {
+//     width: "100%",
+//   },
+//   [theme.breakpoints.down("lg")]: {
+//     fontSize: "1.2rem",
+//   },
+//   [theme.breakpoints.down("md")]: {
+//     fontSize: "0.95rem",
+//     letterSpacing: "1.2px",
+//   },
+//   [theme.breakpoints.down("sm")]: {
+//     fontSize: "0.7rem",
+//     letterSpacing: "1px",
+//     margin: theme.spacing(0),
+//   },
+// }));
+
+// const StyledAvatar = styled(Avatar)(({ theme }) => ({
+//   width: "36px",
+//   height: "36px",
+//   border: "2px solid white",
+//   transition: "box-shadow 0.3s ease-in-out",
+//   "&:hover": {
+//     boxShadow:
+//       "0 4px 8px 0 rgba(255, 255, 255, 0.40), 0 6px 20px 0 rgba(255, 255, 255, 0.19)",
+//   },
+
+//   [theme.breakpoints.down("lg")]: {
+//     width: "33px",
+//     height: "33px",
+//   },
+//   [theme.breakpoints.down("md")]: {
+//     width: "25px",
+//     height: "25px",
+//   },
+//   [theme.breakpoints.down("sm")]: {
+//     width: "20px",
+//     height: "20px",
+//     border: "1px solid white",
+//   },
+// }));
+
+// const Navbar = () => {
+//   const [user, setUser] = useState<User | null>(null);
+//   const [doctorImage, setDoctorImage] = useState<string>("");
+
+//   useEffect(() => {
+//     const authListener = supabase.auth.onAuthStateChange((event, session) => {
+//       if (event === "SIGNED_IN") {
+//         setUser(session?.user ?? null);
+//         fetchUserImage(session?.user.email);
+//       } else if (event === "SIGNED_OUT") {
+//         setUser(null);
+//         setDoctorImage("");
+//       }
+//     });
+
+//     return () => {
+//       // authListener?.unsubscribe();
+//     };
+//   }, []);
+
+//   const fetchUserImage = async (userEmail: string | undefined) => {
+//     if (userEmail) {
+//       const { data: doctorData, error: doctorError } = await supabase
+//         .from("doctorInfo")
+//         .select("img")
+//         .eq("email", userEmail)
+//         .single();
+
+//       const { data: patientData, error: patientError } = await supabase
+//         .from("patientInfo")
+//         .select("img")
+//         .eq("email", userEmail)
+//         .single();
+
+//       if (doctorError && patientError) {
+//         console.error("Error fetching user image:", doctorError, patientError);
+//       } else {
+//         setDoctorImage(doctorData?.img || patientData?.img || "");
+//       }
+//     }
+//   };
+
+//   return (
+//     <NavContainer>
+//       <Box
+//         sx={{
+//           display: "flex",
+//           alignItems: "center",
+//           cursor: "pointer",
+//         }}
+//       >
+//         <MedicineStyle>
+//           <LogoAnimation />
+//         </MedicineStyle>
+//         <LogoLink to="/">HealthHarbor</LogoLink>
+//       </Box>
+
+//       <Box sx={{ display: "flex" }}>
+//         <StyledLink to="/find-a-doctor">Find a Doctor</StyledLink>
+//         <StyledLink to="/service">Services</StyledLink>
+//         <StyledLink to="/about-us">About us</StyledLink>
+//         <StyledLink to="/contact">Contact</StyledLink>
+//       </Box>
+
+//       {user ? (
+//         <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+//           <StyledAvatar
+//             component={Link}
+//             to={"/profile"}
+//             src={doctorImage || ""}
+//           />
+//         </Box>
+//       ) : (
+//         <StyledLink to="/login">Signin</StyledLink>
+//       )}
+//     </NavContainer>
+//   );
+// };
+
+// export default Navbar;
--- src/pages/profile/patientDetails/patientAppointment/PatientAppointment.tsx
+++ src/pages/profile/patientDetails/patientAppointment/PatientAppointment.tsx
@@ -1,24 +1,85 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
-import React from "react";
-const MainContainer = styled(Box)(({ theme }) => ({
+import React, { useEffect, useState } from "react";
+import { supabase } from "../../../../supabase/config";
+
+const MainContainer = styled(Box)(() => ({
   width: "50%",
   margin: "2rem 0",
-  [theme.breakpoints.down("lg")]: {
-    margin: "0",
-  },
-  [theme.breakpoints.down("md")]: {
-    margin: "0",
-  },
-  [theme.breakpoints.down("sm")]: {
-    margin: "0",
-  },
+}));
+
+const TableContainer = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "column",
+}));
+
+const Row = styled(Box)(() => ({
+  display: "flex",
+  justifyContent: "space-around",
+  padding: "0.5rem 0",
+}));
+
+const Cell = styled(Box)(() => ({
+  flex: 1,
+  textAlign: "center",
+  fontSize: "12px",
+  letterSpacing: "0.5px",
 }));
 
 function PatientAppointment({ user }) {
+  const [appointments, setAppointments] = useState();
+
+  useEffect(() => {
+    const fetchPatientInfo = async () => {
+      if (user) {
+        try {
+          const { data, error } = await supabase
+            .from("patientInfo")
+            .select("appointment")
+            .eq("email", user.email)
+            .single();
+          // console.log(data);
+
+          if (error) {
+            console.error("Error fetching patientInfo:", error);
+          } else {
+            setAppointments(data);
+          }
+        } catch (error) {
+          console.error("Error fetching patientInfo:", error);
+        }
+      }
+    };
+    fetchPatientInfo();
+  }, []);
   return (
     <MainContainer>
-      patient appointments -- <b>currently in development phase.</b>
+      <TableContainer>
+        <Row
+          sx={{
+            backgroundColor: "#c3c9fa59",
+            borderTopRightRadius: "10px",
+            borderTopLeftRadius: "10px",
+          }}
+        >
+          <Cell>Doctor Email</Cell>
+          <Cell>Doctor Name</Cell>
+          <Cell>Payment</Cell>
+          <Cell>Appointment Date</Cell>
+          <Cell> Approve</Cell>
+        </Row>
+        {/* {appointments.map((appointment, index) => (
+          <Row key={index} sx={{ border: "1px solid black" }}>
+            <Cell>{appointment.doctorEmail}</Cell>
+            <Cell></Cell>
+            <Cell></Cell>
+            <Cell></Cell>
+            <Cell>
+              {appointment.appointmentDate} _ {appointment.appointmentTime}
+            </Cell>
+          </Row>
+        ))} */}
+      </TableContainer>
     </MainContainer>
   );
 }
